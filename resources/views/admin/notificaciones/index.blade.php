bfdfa0713b8455ac72d048f7f8cc7f45461a2554
webmaster <aleox2407@gmail.com>
2018-02-16 16:55:12 -0600

    agregando plantilla nueva al frontend

--- resources/views/admin/notificaciones/index.blade.php
+++ resources/views/admin/notificaciones/index.blade.php
@@ -52,16 +52,23 @@
                                 <table class="table table-hover table-striped">
                                     <tbody>
 
-                                    @foreach($unreadNotifications as $unreadNotification)
+                                    <ul class="list-group-item">
+                                        @foreach($unreadNotifications as $unreadNotification)
+                                            <li class="list-group-item">
 
-                                        <td class="mailbox-name"><a href="{{($unreadNotification->data['link'])}}">{{($unreadNotification->data['text'])}}</a></td>
-                                        <td class="mailbox-date"> <form method="POST" action="{{route('notificaciones.read', $unreadNotification->id)}}" class="pull-right">
-                                                {{method_field('PATCH')}}
-                                                {{csrf_field()}}
-                                                <button class="btn btn-danger btn-xs">X</button>
-                                            </form></td>
+                                                <a href="{{($unreadNotification->data['link'])}}">
+                                                    {{($unreadNotification->data['text'])}}
+                                                </a>
+                                                <form method="POST" action="{{route('notificaciones.read', $unreadNotification->id)}}" class="pull-right">
+                                                    {{method_field('PATCH')}}
+                                                    {{csrf_field()}}
+                                                    <button class="btn btn-danger btn-xs">X</button>
+                                                </form>
 
-                                    @endforeach
+                                            </li>
+
+                                        @endforeach
+                                    </ul>
 
                                     </tbody>
                                 </table>
@@ -97,16 +104,21 @@
                                 <table class="table table-hover table-striped">
                                     <tbody>
 
-                                    @foreach($readNotifications as $readNotification)
+                                    <ul class="list-group-item">
+                                        @foreach($readNotifications as $readNotification)
+                                            <li class="list-group-item">
+                                                <a href="{{($readNotification->data['link'])}}">
+                                                    {{($readNotification->data['text'])}}
+                                                </a>
+                                                <form method="POST" action="{{route('notificaciones.destroy', $readNotification->id)}}" class="pull-right">
+                                                    {{method_field('DELETE')}}
+                                                    {{csrf_field()}}
+                                                    <button class="btn btn-danger btn-xs" ><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                                                </form>
+                                            </li>
 
-                                        <td class="mailbox-name"><a href="{{($readNotification->data['link'])}}">{{($readNotification->data['text'])}}</a></td>
-                                        <td class="mailbox-date"> <form method="POST" action="{{route('notificaciones.destroy', $readNotification->id)}}" class="pull-right">
-                                                {{method_field('DELETE')}}
-                                                {{csrf_field()}}
-                                                <button class="btn btn-danger btn-xs" ><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-                                            </form></td>
-
-                                    @endforeach
+                                        @endforeach
+                                    </ul>
 
 
                                     </tbody>
